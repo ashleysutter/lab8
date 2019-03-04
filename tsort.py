115daa885fcae5d8ac1274cd088811e274ec8645
ashleysutter <36204775+ashleysutter@users.noreply.github.com>
2019-03-04 08:36:06 -0800

    lab 8 starter code

---
+++ tsort.py
@@ -0,0 +1,43 @@
+from sys import argv
+from stack_array import *
+
+def tsort(vertices):
+    '''
+    * Performs a topological sort of the specified directed acyclic graph.  The
+    * graph is given as a list of vertices where each pair of vertices represents
+    * an edge in the graph.  The resulting string return value will be formatted
+    * identically to the Unix utility {@code tsort}.  That is, one vertex per
+    * line in topologically sorted order.
+    *
+    * Raises a ValueError if:
+    *   - vertices is emtpy with the message "input contains no edges"
+    *   - vertices has an odd number of vertices (incomplete pair) with the
+    *     message "input contains an odd number of tokens"
+    *   - the graph contains a cycle (isn't acyclic) with the message 
+    *     "input contains a cycle"'''
+    pass
+
+def main():
+    '''Entry point for the tsort utility allowing the user to specify
+       a file containing the edge of the DAG'''
+    if len(argv) != 2:
+        print("Usage: python3 tsort.py <filename>")
+        exit()
+    try:
+        f = open(argv[1], 'r')
+    except FileNotFoundError as e:
+        print(argv[1], 'could not be found or opened')
+        exit()
+    
+    vertices = []
+    for line in f:
+        vertices += line.split()
+       
+    try:
+        result = tsort(vertices)
+        print(result)
+    except Exception as e:
+        print(e)
+    
+if __name__ == '__main__': 
+    main()
